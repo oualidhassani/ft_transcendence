41b47fe6164ea278668bea58fab31f484fd17c7f
3IMAD69 <dhaimy.imad@gmail.com>
2025-09-18 17:37:10 +0100

    at least working on local

--- Shared_dataBase/database/db-connection.js
+++ Shared_dataBase/database/db-connection.js
@@ -1,32 +1,24 @@
-import sqlite3 from 'sqlite3';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import path, { dirname } from "path";
+import sqlite3 from "sqlite3";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const sqlite = sqlite3.verbose();
 
+class Database {
+  constructor() {
+    const db_path = path.join(__dirname, "../../database/transcendence.db");
+    this.db = new sqlite.Database(db_path, (err) => {
+      if (err) console.error("Database opening error: ", err);
+      else console.log("Connecting to database ");
+    });
+    this.initTables();
+  }
 
-class Database 
-{
-    constructor() 
-    {
-    const db_path = path.join('/app/database', 'transcendence.db');
-        this.db = new sqlite.Database(db_path, (err) =>
-        {
-            if (err)
-                console.error('Database opening error: ', err);
-            else
-                console.log("Connecting to database ");
-        })         
-        this.initTables()
-    }
-
-    initTables() 
-    {
-        this.db.run(`
+  initTables() {
+    this.db.run(`
           CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT UNIQUE NOT NULL,
@@ -36,73 +28,57 @@ class Database
             is_42_user BOOLEAN DEFAULT 0,
             created_at DATETIME DEFAULT CURRENT_TIMESTAMP
           )
-        `)
-    
-    }
+        `);
+  }
 
-    createUser(username, email, password)
-    {
-        return new Promise((resolve, reject)=>
-        {
-            this.db.run('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, password],
-                function(err)
-                {
-                    if(err)
-                    {
-                        reject(err);
-                    }
-                    else
-                    {
-                        const userid = this.lastID
-                        resolve({id: userid, username, email})
-                    }
-                }
-            )
-        })
-    }
+  createUser(username, email, password) {
+    return new Promise((resolve, reject) => {
+      this.db.run(
+        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+        [username, email, password],
+        function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            const userid = this.lastID;
+            resolve({ id: userid, username, email });
+          }
+        }
+      );
+    });
+  }
 
-    findUserByUsername(username) {
-        return new Promise((resolve, reject) => 
-            {
-            this.db.get(
-                'SELECT * FROM users WHERE username = ?',
-                [username],
-                function(err, row) 
-                {
-                    if (err) reject(err);
-                    else resolve(row);
-                }
-            )
-        });
-    }
-    
-    findEmailByEmail(email)
-    {
-            return new Promise((resolve, reject) => 
-            {
-            this.db.get(
-                'SELECT * FROM users WHERE email = ?',
-                [email],
-                function(err, row) 
-                {
-                    if (err) reject(err);
-                    else resolve(row);
-                }
-            );
-            });
-    }
-    close () 
-    {
-        this.db.close((err) => 
-        {
-            if (err) 
-                console.error('Error closing the database connection:', err);
-            else 
-                console.log('Database connection closed.');
-        });
-    }
+  findUserByUsername(username) {
+    return new Promise((resolve, reject) => {
+      this.db.get(
+        "SELECT * FROM users WHERE username = ?",
+        [username],
+        function (err, row) {
+          if (err) reject(err);
+          else resolve(row);
+        }
+      );
+    });
+  }
 
+  findEmailByEmail(email) {
+    return new Promise((resolve, reject) => {
+      this.db.get(
+        "SELECT * FROM users WHERE email = ?",
+        [email],
+        function (err, row) {
+          if (err) reject(err);
+          else resolve(row);
+        }
+      );
+    });
+  }
+  close() {
+    this.db.close((err) => {
+      if (err) console.error("Error closing the database connection:", err);
+      else console.log("Database connection closed.");
+    });
+  }
 }
 
 export default new Database();
-
--- services/auth-service/server.js
+++ services/auth-service/server.js
@@ -1,112 +1,95 @@
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import bcrypt from "bcrypt";
+import Fastify from "fastify";
+import db from "./Shared_dataBase/database/db-connection.js";
 
-import Fastify from 'fastify';
-import bcrypt from 'bcrypt';
-import cors from '@fastify/cors';
-import jwt from '@fastify/jwt';
-import db from './Shared_dataBase/database/db-connection.js'; 
-
-const app = Fastify(
-  {
-    logger : true 
-  }
-);
+const app = Fastify({
+  logger: true,
+});
 // const bcrypt = require('bcrypt');
 // const db = require('../../Shared_dataBase/database/db-connection.js');
 
 await app.register(cors);
-await app.register(jwt, 
-{
-  secret: process.env.JWT_SECRET || 'transcendence-secret-key'
+await app.register(jwt, {
+  secret: process.env.JWT_SECRET || "transcendence-secret-key",
 });
 
-app.get('/test', async(request , reply)=>
-{
+app.get("/test", async (request, reply) => {
   console.log("hey im here working good ");
-})
+});
 
-app.post('/register', async(request , reply)=>
-{
-    const   {username , email, password} =  request.body;
-
-    try 
-    {
-        const existingUser = await db.findUserByUsername(username);
-        if(existingUser)
-        {
-            return reply.status(400).send({error: 'Username already exists'});
-        }
-        const existingEmail = await db.findEmailByEmail(email);
-        if(existingEmail)
-        {
-            return reply.status(400).send({error: 'Email already exists'});
-        }
-      
-        const hashPassword = await bcrypt.hash(password, 12);
-        
-        const newUser = await db.createUser(username, email, hashPassword);
-
-        console.log("New user is created ", newUser);
-        
-        return { 
-          message: 'User registered successfully', 
-          newUser: { id: newUser.id, username: newUser.username }
-        };
-
-        
-    }
-    catch(err)
-    {
-      console.error('Login error:', err)
-      reply.code(500).send({ error: 'Login failed' })
-    }
-})
-
-
-app.post('/login', async (request, reply) => {
-  const { username, password } = request.body
+app.post("/register", async (request, reply) => {
+  const { username, email, password } = request.body;
 
   try {
-    const user = await db.findUserByUsername(username)
-    if (!user) {
-      return reply.code(401).send({ error: 'Invalid credentials' })
+    const existingUser = await db.findUserByUsername(username);
+    if (existingUser) {
+      return reply.status(400).send({ error: "Username already exists" });
+    }
+    const existingEmail = await db.findEmailByEmail(email);
+    if (existingEmail) {
+      return reply.status(400).send({ error: "Email already exists" });
     }
 
-    const isValidPassword = await bcrypt.compare(password, user.password)
+    const hashPassword = await bcrypt.hash(password, 12);
+
+    const newUser = await db.createUser(username, email, hashPassword);
+
+    console.log("New user is created ", newUser);
+
+    return {
+      message: "User registered successfully",
+      newUser: { id: newUser.id, username: newUser.username },
+    };
+  } catch (err) {
+    console.error("Login error:", err);
+    reply.code(500).send({ error: "Login failed" });
+  }
+});
+
+app.post("/login", async (request, reply) => {
+  const { username, password } = request.body;
+
+  try {
+    const user = await db.findUserByUsername(username);
+    if (!user) {
+      return reply.code(401).send({ error: "Invalid credentials" });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
     if (!isValidPassword) {
-      return reply.code(401).send({ error: 'Invalid credentials' })
+      return reply.code(401).send({ error: "Invalid credentials" });
     }
 
     const token = app.jwt.sign(
       { userId: user.id, username: user.username },
-      { expiresIn: '7d' }
-    )
+      { expiresIn: "7d" }
+    );
 
     return {
       token,
       user: {
         id: user.id,
         username: user.username,
-        email: user.email
-      }
-    }
+        email: user.email,
+      },
+    };
   } catch (error) {
-    console.error('Login error:', error)
-    reply.code(500).send({ error: 'Login failed' })
+    console.error("Login error:", error);
+    reply.code(500).send({ error: "Login failed" });
   }
-})
+});
 
 const start = async () => {
-  try 
-  {
-    await app.listen({ port: 3001, host: '0.0.0.0' })
-    console.log(' Auth Service running on port 3001')
-    console.log(' Database: SQLite (shared)')
-  } 
-  catch (err) 
-  {
-    app.log.error(err)
-    process.exit(1)
+  try {
+    await app.listen({ port: 3010, host: "0.0.0.0" });
+    console.log(" Auth Service running on port 3001");
+    console.log(" Database: SQLite (shared)");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
   }
-}
+};
 
-start()
+start();
